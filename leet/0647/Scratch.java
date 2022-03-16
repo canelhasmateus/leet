da03bc9a00ea907ef3659f123ee60d0c4c223cc6
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-03-16 20:00:42 -0300

    better comparison

--- leet/0647/Scratch.java
+++ leet/0647/Scratch.java
@@ -1,21 +1,41 @@
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
 
 class Scratch {
 
-    public static final String reverse( String  s ) {
+    public static final < T > void compare( T actual, T expected ) {
+
+
+        if ( !expected.equals( actual ) ) {
+
+            throw new RuntimeException( "Error!\nexpected:\n\t\t" +
+                                        expected +
+                                        "\ngotten:\n\t\t" +
+                                        actual );
+
+        }
+    }
+
+    public static final String reverse( String s ) {
         return new StringBuilder( s ).reverse().toString();
     }
 
-    public static final boolean isPalidrome(String s) {
-        return s.equals( reverse( s) );
+    public static final boolean isPalidrome( String s ) {
+        return s.equals( reverse( s ) );
     }
 
     public static final List< String > palidromicSubstring( String s ) {
-        return Arrays.stream( s.split(  "" ) ).toList();
+        return Arrays.stream( s.split( "" ) ).toList();
     }
+
     public static void main( String[] args ) {
 
-        assert( palidromicSubstring( "abc" ) == List.of("a" , "b" , "c"));
+        compare( palidromicSubstring( "abc" ),
+                 List.of( "a", "b", "c" )
+               );
+
+
+
     }
 }
