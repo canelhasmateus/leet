72c46a1985761d33809a43235633fd9bd091a064
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-03-16 20:15:22 -0300

    Added second 0647 test

--- leet/0647/Scratch.java
+++ leet/0647/Scratch.java
@@ -1,11 +1,12 @@
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 class Scratch {
 
     public static final < T > void compare( T actual, T expected ) {
-
 
         if ( !expected.equals( actual ) ) {
 
@@ -22,11 +23,34 @@ class Scratch {
     }
 
     public static final boolean isPalidrome( String s ) {
-        return s.equals( reverse( s ) );
+        return s.length() > 0 && s.equals( reverse( s ) );
+    }
+
+    public static final List< String > substrings( String s ) {
+
+        int            length = s.length();
+        List< String > res    = new ArrayList<>();
+        for ( int i = 0; i < length; i++ ) {
+
+            for ( int j = i; j < length; j++ ) {
+
+                String substring = s.substring( i, j + 1 );
+                res.add( substring );
+
+            }
+
+        }
+
+        return res;
+
     }
 
     public static final List< String > palidromicSubstring( String s ) {
-        return Arrays.stream( s.split( "" ) ).toList();
+
+        return substrings( s )
+                .stream()
+                .filter( Scratch::isPalidrome )
+                .toList();
     }
 
     public static void main( String[] args ) {
@@ -35,6 +59,15 @@ class Scratch {
                  List.of( "a", "b", "c" )
                );
 
+        compare( palidromicSubstring( "aaa" ).stream().sorted().toList(),
+                 List.of( "a",
+                          "a",
+                          "a",
+                          "aa",
+                          "aa",
+                          "aaa"
+                        ).stream().toList().stream().sorted().toList()
+               );
 
 
     }
