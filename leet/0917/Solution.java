896a11d797538f4846f8d928761a66949312a873
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-03-21 18:41:04 -0300

    green

--- leet/0917/Solution.java
+++ leet/0917/Solution.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 class Solution {
 
 
@@ -14,25 +21,58 @@ class Solution {
     }
 
     public static final String reverse( String s ) {
-        return new StringBuffer(s).reverse().toString();
+        return new StringBuffer( s ).reverse().toString();
     }
-    
-    public static final String reverseOnly( String s) {
-        return reverse( s ) ;
+
+    public static boolean isLetter( char i ) {
+
+        return ( i >= 'a' && i <= 'z' ) || ( i >= 'A' && i <= 'Z' );
+
     }
+
+    public static final String reverseOnly( String s ) {
+
+        int p1 = 0;
+        int p2 = s.length() - 1;
+
+        StringBuilder sb = new StringBuilder( s.toString() );
+
+        while ( p1 < p2 ) {
+
+            char left  = sb.charAt( p1 );
+            char right = sb.charAt( p2 );
+
+            if ( isLetter( left ) ) {
+                if ( isLetter( right ) ) {
+                    sb.setCharAt( p1, right );
+                    sb.setCharAt( p2, left );
+                    p1++;
+                    p2--;
+                }
+                else {
+                    p2--;
+                }
+            }
+            else {
+                p1++;
+            }
+        }
+
+        return sb.toString();
+    }
+
     public static void main( String[] args ) {
 
 
-        compare(  reverseOnly( "ab-cd" ) , "dc-ba");
-        compare(  reverseOnly(  "a-bC-dEf-ghIj" ) , "j-Ih-gfE-dCba");
+        compare( reverseOnly( "ab-cd" ), "dc-ba" );
+        compare( reverseOnly( "a-bC-dEf-ghIj" ), "j-Ih-gfE-dCba" );
 
     }
-    
-    
-    
+
+
     public String reverseOnlyLetters( String s ) {
-        
-        return reverseOnly( s ); 
-        
+
+        return reverseOnly( s );
+
     }
 }
