4038f37a4161bbeb419eab95eaaeec44153228ea
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-03-21 17:19:07 -0300

    917 green

---
+++ leet/0917/Solution.java
@@ -0,0 +1,36 @@
+class Solution {
+
+
+    public static final < T > void compare( T actual, T expected ) {
+
+        if ( !expected.equals( actual ) ) {
+
+            throw new RuntimeException( "Error!\nexpected:\n\t\t" +
+                                        expected +
+                                        "\ngotten:\n\t\t" +
+                                        actual );
+
+        }
+    }
+    
+    public static final String reverse( String s ) {
+        return new StringBuffer(s).reverse().toString();
+    }
+    
+    public static final String reverseOnly( String s) {
+        return reverse( s ) ;
+    }
+    public static void main( String[] args ) {
+        
+        compare(  reverseOnly( "ab-cd" ) , "dc-ba");
+        
+    }
+    
+    
+    
+    public String reverseOnlyLetters( String s ) {
+        
+        return reverseOnly( s ); 
+        
+    }
+}
