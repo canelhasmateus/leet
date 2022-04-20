ee0100b7f1af7e363fd297575bece4f9ee25bf42
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-20 12:45:23 -0300

    auto: Tdd Refactor

--- signal/002-edge/adjacentProduct.py
+++ signal/002-edge/adjacentProduct.py
@@ -3,7 +3,8 @@ def solution( array ):
 	maxProd = 0
 	for current in array:
 		prod = current * prev
-		if (prod > maxProd) :
+		maxProd = prod if prod > maxProd else maxProd
+		if (prod > maxProd):
 			maxProd = prod
 		prev = current
 	return maxProd
@@ -16,7 +17,7 @@ if __name__ == '__main__':
 	class TestSolution( unittest.TestCase ):
 
 		def test1( self ):
-			assert solution( [ 3 , 6 , -2 , -5 , 7 , 3 ] )  == 21
+			assert solution( [ 3, 6, -2, -5, 7, 3 ] ) == 21
 
 
 	unittest.main()
