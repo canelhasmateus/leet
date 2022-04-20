a9664beb92a2755fee15351d1319df67c05f85c2
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-20 16:03:43 -0300

    auto: Tdd Green

--- signal/002-edgeofsea/almostIncreasing.py
+++ signal/002-edgeofsea/almostIncreasing.py
@@ -25,7 +25,7 @@ def solution( param1 ):
 
 	for i in range(len(param1)):
 		element = tmp[i]
-		tmp[i] = get( tmp , i - 1 , -1 )
+		tmp[i] = get( tmp , i - 1 , 0 ) + 0.01
 
 		if is_increasing( tmp ):
 			return True
