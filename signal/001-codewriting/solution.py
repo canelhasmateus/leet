519becb0e5a3afa16cd3cc32bdadd7b24605f092
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-20 12:17:35 -0300

    auto: Tdd Red

--- signal/001-codewriting/solution.py
+++ signal/001-codewriting/solution.py
@@ -1,8 +1,7 @@
 
 
 def solution( param1, param2 ):
-	x = 3
-	return x
+	return param1 + param2
 
 
 if __name__ == '__main__':
@@ -13,6 +12,9 @@ if __name__ == '__main__':
 
 		def test1( self ):
 			assert solution( 1 ,2 ) == 3
+
+		def test2( self ):
+			assert solution( 2 , 3 ) == 4
 
 
 	unittest.main()
