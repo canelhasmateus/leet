3c37a7eda013788cf6002f568779360b8c95f8a2
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-20 12:48:10 -0300

    auto: Tdd Red

--- signal/002-edge/adjacentProduct.py
+++ signal/002-edge/adjacentProduct.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
 			assert solution( [ -23, 4, -3, 8, -12 ] ) == -12
 
 		def test3( self ):
-			assert solution( [ 1, 0 , 1 , 0 , 1000 ] ) == 1
+			assert solution( [ 1, 0 , 1 , 0 , 1000 ] ) == 0
 
 
 	unittest.main()
