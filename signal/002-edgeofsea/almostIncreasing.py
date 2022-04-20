b29b705074369bc64b0ed58e40292be2ea4e571b
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-20 15:52:36 -0300

    auto: Tdd Refactor

--- signal/002-edgeofsea/almostIncreasing.py
+++ signal/002-edgeofsea/almostIncreasing.py
@@ -11,12 +11,16 @@ def is_increasing( param1 ):
 
 def solution( param1 ):
 	tmp = [ i for i in param1]
+	if not is_increasing( sorted( param1 ) ):
+		return False
+	
 	for i in range(len(param1)):
 		element = tmp.pop( i )
 		if is_increasing( tmp ):
 			return True
 
 		tmp.insert( i , element)
+
 	return False
 
 
