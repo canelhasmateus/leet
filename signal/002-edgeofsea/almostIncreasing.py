ca675f10de705cc414e2e740a9895a3e478c569b
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-20 16:13:10 -0300

    auto: Tdd Refactor

--- signal/002-edgeofsea/almostIncreasing.py
+++ signal/002-edgeofsea/almostIncreasing.py
@@ -10,31 +10,35 @@ def is_increasing( param1 ):
 	return True
 
 
-def get( tmp , i  , n ) :
-
-	try :
+def get( tmp, i, n ):
+	try:
 		if 0 <= i <= len( tmp ):
-			return tmp[i]
+			return tmp[ i ]
 		return n
 	except:
 		return n
 
+
 def solution( param1 ):
-	tmp = [ i for i in param1]
+	tmp = [ i for i in param1 ]
 
+	for i in range( len( param1 ) ):
+		element = tmp[ i ]
 
-	for i in range(len(param1)):
-		element = tmp[i]
-		tmp[i] = get( tmp , i + 1 , get( tmp , i - 1 , 0) ) - 0.5
+		if i == 0:
+			tmp[ i ] = tmp[ i + 1 ] - 1
+
+		elif i == len( param1 ) - 1:
+			tmp[ i ] = tmp[ i - 1 ] + 1
+		else:
+			tmp[ i ] = (tmp[ i - 1 ] + tmp[ i + 1 ]) / 2
 
 		if is_increasing( tmp ):
 			return True
 
-		tmp[i] = element
-
+		tmp[ i ] = element
 
 	return False
-
 
 
 if __name__ == '__main__':
@@ -43,8 +47,6 @@ if __name__ == '__main__':
 
 	class TestSolution( unittest.TestCase ):
 
-		def test0( self ):
-			self.assertEquals( solution( [ 1 ] ), True )
 
 		def test1( self ):
 			self.assertEquals( solution( [ 1, 3, 2, 1 ] ), False )
@@ -56,7 +58,7 @@ if __name__ == '__main__':
 			self.assertEquals( solution( [ 1, 2, 1, 2 ] ), False )
 
 		def test4( self ):
-			self.assertEquals( solution( [10, 1, 2, 3, 4, 5] ), True )
+			self.assertEquals( solution( [ 10, 1, 2, 3, 4, 5 ] ), True )
 
 
 	unittest.main()
