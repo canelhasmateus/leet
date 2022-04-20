a434485b45f745d435ef493fb9b834562fe43c18
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-20 13:36:39 -0300

    auto: Tdd Red

--- signal/002-edgeofsea/makeArrayConsecutive2.py
+++ signal/002-edgeofsea/makeArrayConsecutive2.py
@@ -1,5 +1,5 @@
 def solution( param1, param2 ):
-	...
+	return param1
 
 
 if __name__ == '__main__':
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 	class TestSolution( unittest.TestCase ):
 
 		def test1( self ):
-			self.assertEquals( False, True )
+			self.assertEquals( solution( [ 6 , 2 , 3 , 8 ] ) , 3 )
 
 
 	unittest.main()
