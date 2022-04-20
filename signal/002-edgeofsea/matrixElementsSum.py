1b62dcc99160fb7bb4d46411e22fa96954717f7e
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-20 16:52:42 -0300

    auto: Tdd Red

--- signal/002-edgeofsea/matrixElementsSum.py
+++ signal/002-edgeofsea/matrixElementsSum.py
@@ -1,9 +1,10 @@
 def solution( matrix ):
 	haunted = set()
 	res = 0
+
 	for i, row in enumerate( matrix ):
 		for j, cost in enumerate( row ):
-			
+
 			if cost == 0:
 				haunted.add( j )
 
@@ -25,6 +26,12 @@ if __name__ == '__main__':
 			           [ 2, 0, 3, 3 ] ]
 
 			self.assertEquals( solution( matrix ), 9 )
+		def test1( self ):
+			matrix = [[1, 1, 1, 0],
+			          [0, 5, 0, 1],
+			          [2, 1, 3, 10]]
+
+			self.assertEquals( solution( matrix ), 8 )
 
 
 	unittest.main()
