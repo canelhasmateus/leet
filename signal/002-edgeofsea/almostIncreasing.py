8208788bc9c767e9888b38ccd0542988985c5e3c
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-20 13:43:20 -0300

    auto: Tdd Red

---
+++ signal/002-edgeofsea/almostIncreasing.py
@@ -0,0 +1,15 @@
+def solution( param1):
+	return param1
+
+
+if __name__ == '__main__':
+	import unittest
+
+
+	class TestSolution( unittest.TestCase ):
+
+		def test1( self ):
+			self.assertEquals( [ 1 , 3 , 2 , 1 ] , False )
+
+
+	unittest.main()
