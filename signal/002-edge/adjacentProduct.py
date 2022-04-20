e721223c197ef7f96ee4fb410c73dcbf03c5c57a
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-20 12:53:07 -0300

    auto: Tdd Red

--- signal/002-edge/adjacentProduct.py
+++ signal/002-edge/adjacentProduct.py
@@ -2,14 +2,13 @@ import math
 
 
 def solution( array ):
-	prev , *rest = array
-
-	maxProd = -math.inf
+	prev, *rest = array
+	max_prod = -math.inf
 	for current in rest:
 		prod = current * prev
-		maxProd = prod if prod > maxProd else maxProd
+		max_prod = prod if prod > max_prod else max_prod
 		prev = current
-	return maxProd
+	return max_prod
 
 
 if __name__ == '__main__':
@@ -25,7 +24,7 @@ if __name__ == '__main__':
 			assert solution( [ -23, 4, -3, 8, -12 ] ) == -12
 
 		def test3( self ):
-			assert solution( [ 1, 0 , 1 , 0 , 1000 ] ) == 0
+			assert solution( [ 1, 0, 1, 0, 1000 ] ) == 0
 
 
 	unittest.main()
---
+++ signal/002-edge/shapeArea.py
@@ -0,0 +1,15 @@
+def solution( param1):
+	return 0
+
+
+if __name__ == '__main__':
+	import unittest
+
+
+	class TestSolution( unittest.TestCase ):
+
+		def test1( self ):
+			assert solution( 2 ) == 5
+
+
+	unittest.main()
