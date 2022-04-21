a71cf5a611d519caf7f1c2a4818f26db5595b46c
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-21 11:02:13 -0300

    auto: Tdd Red

--- signal/002-edgeofsea/matrixElementsSum.py
+++ signal/002-edgeofsea/matrixElementsSum.py
@@ -1,38 +1,44 @@
+import hypothesis.strategies as st
+import pytest
+from hypothesis import given, example, settings, Verbosity
+from hypothesis.stateful import precondition
+
+
 def solution( matrix ):
 	haunted = set()
 	res = 0
 
-	for i, row in enumerate( matrix ):
-		for j, cost in enumerate( row ):
+	for j in range( len( matrix[ 0 ] ) ):
+		for i in range( len( matrix ) ):
+
+			cost = matrix[ i ][ j ]
 
 			if cost == 0:
-				haunted.add( j )
+				break
 
-			if j not in haunted:
-				res += cost
+			res += cost
 
 	return res
 
 
+@settings( verbosity=Verbosity.verbose )
+@given( st.integers(), st.integers() )
+def test_1( a, b ):
+	precondition( lambda : a > 10)
+	if not a > 10:
+		assert False
+
+	assert True
+
+
+@given( st.integers(), st.integers() )
+def test_2( a, b ):
+	matrix = [ [ 1, 1, 1, 0 ],
+	           [ 0, 5, 0, 1 ],
+	           [ 2, 1, 3, 10 ] ]
+
+	assert solution( matrix ) == 9
+
+
 if __name__ == '__main__':
-	import unittest
-
-
-	class TestSolution( unittest.TestCase ):
-
-		def test1( self ):
-			matrix = [ [ 0, 1, 1, 2 ],
-			           [ 0, 5, 0, 0 ],
-			           [ 2, 0, 3, 3 ] ]
-
-			self.assertEquals( solution( matrix ), 9 )
-
-		def test1( self ):
-			matrix = [[1, 1, 1, 0],
-			          [0, 5, 0, 1],
-			          [2, 1, 3, 10]]
-
-			self.assertEquals( solution( matrix ), 9 )
-
-
-	unittest.main()
+	pytest.main( [ __file__, "-v" , "-s" ] )
