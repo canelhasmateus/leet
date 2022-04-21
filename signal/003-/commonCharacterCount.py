b4da76586302d0da45a7ec28d01688ad5f70d463
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-21 11:19:27 -0300

    auto: Tdd Green

--- signal/003-/commonCharacterCount.py
+++ signal/003-/commonCharacterCount.py
@@ -1,11 +1,21 @@
 def solution( param1, param2 ):
 
-	count = 0
-	for left , right in zip( param1 , param2):
-		if left == right:
-			count+=1
 
-	return count
+	counts = {}
+
+	for left in param1:
+		counts[left] = counts.get( left , 0) + 1
+
+	for right in param2:
+		counts[right] = max( counts.get( right , 0) - 1 , 0)
+
+
+	total = 0
+	for val in counts.values():
+		total += val
+
+
+	return len( param1 ) - total
 
 
 if __name__ == '__main__':
