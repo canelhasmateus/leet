fe12fc968b7b64de1be69149adebc0b1e25bcb77
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-21 11:22:21 -0300

    auto: Tdd Refactor

--- signal/003-/commonCharacterCount.py
+++ signal/003-/commonCharacterCount.py
@@ -1,21 +1,19 @@
 def solution( param1, param2 ):
 
-
 	counts = {}
 
 	for left in param1:
-		counts[left] = counts.get( left , 0) + 1
-
-	for right in param2:
-		counts[right] = max( counts.get( right , 0) - 1 , 0)
-
+		counts[ left ] = counts.get( left, 0 ) + 1
 
 	total = 0
-	for val in counts.values():
-		total += val
+	for right in param2:
+		current_count = counts.get( right, 0 )
+		if current_count > 0:
+			total += 1
+			counts[ right ] = current_count - 1
 
 
-	return len( param1 ) - total
+	return total
 
 
 if __name__ == '__main__':
