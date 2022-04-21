a6b8177d547dae9ea933e1f2bea6bc9a9e328d7a
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-21 11:30:24 -0300

    auto: Tdd Red

--- signal/003-smoothsailing/isLucky.py
+++ signal/003-smoothsailing/isLucky.py
@@ -1,4 +1,19 @@
-def solution( param1):
+def solution( param1 ):
+	digits = str( param1 )
+	size = len( digits )
+	leftSum = 0
+	rightSum = 0
+	for i in range( size ):
+		element = int( digits[ i ] )
+
+		isLeft = (i < size / 2)
+
+		if isLeft:
+			leftSum += element
+		else:
+			rightSum += element
+
+
 	return False
 
 
