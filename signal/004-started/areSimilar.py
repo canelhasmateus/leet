ca3665bbdcd9416adb9d1e08e06a4a96a0947b10
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-22 08:34:58 -0300

    auto: Tdd Green

--- signal/004-started/areSimilar.py
+++ signal/004-started/areSimilar.py
@@ -1,9 +1,13 @@
 def solution( param1, param2 ):
-
 	if param1 == param2:
 		return True
-	
-	return False
+
+	seen = set( param1 )
+	for element in param2:
+		if element not in seen:
+			return False
+
+	return True
 
 
 if __name__ == '__main__':
@@ -13,9 +17,10 @@ if __name__ == '__main__':
 	class TestSolution( unittest.TestCase ):
 
 		def test1( self ):
-			self.assertEquals( solution( [ 1 , 2 , 3 ] , [ 1, 2 , 3]), True )
+			self.assertEquals( solution( [ 1, 2, 3 ], [ 1, 2, 3 ] ), True )
+
 		def test2( self ):
-			self.assertEquals( solution( [ 1 , 2 , 3 ] , [ 2  , 1, 3]), True )
+			self.assertEquals( solution( [ 1, 2, 3 ], [ 2, 1, 3 ] ), True )
 
 
 	unittest.main()
