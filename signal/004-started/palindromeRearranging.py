dc09e83329ec9266160e7af6bed832349315cd3f
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-22 09:41:50 -0300

    auto: Tdd palindromeRearranging.py Green

--- signal/004-started/palindromeRearranging.py
+++ signal/004-started/palindromeRearranging.py
@@ -1,6 +1,13 @@
-def solution( param1):
-	return False
-	
+def solution( param1 ):
+	count = {}
+	for character in param1:
+		count[ character ] = count.get( character , 0 ) + 1
+
+	res = 0
+	for value in count.values():
+		res += value % 2
+
+	return res <= 1
 
 
 if __name__ == '__main__':
@@ -10,7 +17,7 @@ if __name__ == '__main__':
 	class TestSolution( unittest.TestCase ):
 
 		def test1( self ):
-			self.assertEquals( solution( "aabb"), True )
+			self.assertEquals( solution( "aabb" ), True )
 
 
 	unittest.main()
