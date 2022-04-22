b290181f712084586deb5b05c3ee3d52038ff09f
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-22 09:38:42 -0300

    auto: Tdd palindromeRearranging.py Red

---
+++ signal/004-started/palindromeRearranging.py
@@ -0,0 +1,15 @@
+def solution( param1, param2 ):
+	...
+
+
+if __name__ == '__main__':
+	import unittest
+
+
+	class TestSolution( unittest.TestCase ):
+
+		def test1( self ):
+			self.assertEquals( False, True )
+
+
+	unittest.main()
