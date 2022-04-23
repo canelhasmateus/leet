23979eaefbf6c394d05a0f338e0c6baf71e5013c
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-23 11:43:18 -0300

    auto: Tdd avoidObstacles.py Green

--- signal/005-/avoidObstacles.py
+++ signal/005-/avoidObstacles.py
@@ -1,5 +1,18 @@
-def solution( param1 ):
-	return 0
+def solution( obstacles ):
+	i = 0
+	while True:
+		i += 1
+
+		multiple = map( lambda x : x % i == 0, obstacles)
+
+		if not any( multiple ):
+			return i
+	
+
+
+
+
+
 
 if __name__ == '__main__':
 	import unittest
@@ -8,7 +21,7 @@ if __name__ == '__main__':
 	class TestSolution( unittest.TestCase ):
 
 		def test1( self ):
-			self.assertEquals( solution([5,3,6,7,9]), 4 )
+			self.assertEquals( solution( [ 5, 3, 6, 7, 9 ] ), 4 )
 
 
 	unittest.main()
