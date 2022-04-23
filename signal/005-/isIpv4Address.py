8738c296fc88ef1aecae30c28c46dfb8f6623e62
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-23 10:48:58 -0300

    auto: Tdd isIpv4Address.py Red

---
+++ signal/005-/isIpv4Address.py
@@ -0,0 +1,15 @@
+def solution( param1, param2 ):
+	...
+
+
+if __name__ == '__main__':
+	import unittest
+
+
+	class TestSolution( unittest.TestCase ):
+
+		def test1( self ):
+			self.assertEquals( solution( "172.16.254.1"), True )
+
+
+	unittest.main()
