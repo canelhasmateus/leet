f9434f1665178f3596ebc06cb1d449a5d564ecf4
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-23 11:05:42 -0300

    auto: Tdd isIpv4Address.py Green

--- signal/005-/isIpv4Address.py
+++ signal/005-/isIpv4Address.py
@@ -2,7 +2,7 @@ import re
 
 
 def solution( param1 ):
-	digits = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+	digits = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
 	digits = { str( i ) for i in digits }
 
 	splits = param1.split( "." )
