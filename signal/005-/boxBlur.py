af1d918fa4bcf05ed376777a0611c97d10d1fcb8
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-23 11:53:13 -0300

    auto: Tdd boxBlur.py Green

--- signal/005-/boxBlur.py
+++ signal/005-/boxBlur.py
@@ -1,5 +1,28 @@
+import math
+
+
 def solution( photo ):
-	return [[ ]]
+
+	res = []
+
+	for i in range( 1 , len( photo) - 1):
+		row = photo[i]
+		blurred_row = [ ]
+		for j in range( 1 , len( row) - 1 ):
+
+
+			above_below = sum( photo[ i - 1] ) + sum(photo[i  + 1])
+			sides =row[ j - 1 ] +  row[ j + 1]
+			element = row[j]
+			blurred = (above_below + sides + element) / 9
+
+			blurred_row.append( math.floor( blurred) )
+
+
+		res.append( blurred_row)
+
+
+	return res
 
 
 
