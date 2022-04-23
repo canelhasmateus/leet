3ca53bc2851ea41651b882eec93f982ac5a3497a
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-23 12:41:30 -0300

    auto: Tdd minesweeper.py Green

--- signal/005-/minesweeper.py
+++ signal/005-/minesweeper.py
@@ -1,52 +1,31 @@
 import copy
+import itertools
 import math
 
 
-def solution( mines ):
+def solution( matrix ):
 	res = [ ]
-	for i in range( len( mines ) ):
-		res.append( [ 0 for _ in range( len( mines[ 0 ] ) ) ] )
+	height = len( matrix )
+	length = len( matrix[ 0 ] )
+	for i in range( height ):
+		res.append( [ 0 for _ in range( length ) ] )
 
-	for i in range( len( mines ) ):
-		for j in range( len( mines[ 0 ] ) ):
-			if mines[ i ][ j ]:
-				res[ i ][ j ] += 1
-				res[ i ][ j + 1 ] += 1
-				res[ i ][ j - 1 ] += 1
-				res[ i + 1 ][ j ] += 1
-				res[ i + 1 ][ j + 1 ] += 1
-				res[ i + 1 ][ j - 1 ] += 1
-				res[ i - 1 ][ j ] += 1
-				res[ i - 1 ][ j + 1 ] += 1
-				res[ i - 1 ][ j - 1 ] += 1
+	for i in range( height ):
+		for j in range( length ):
+			if matrix[ i ][ j ]:
+				for r, c in itertools.product( (-1, 0, 1), (-1, 0, 1) ):
+					if r == c == 0:
+						continue
+						
+					newI = i + r
+					newJ = j + c
+
+
+					if 0 <= newI < height and 0 <= newJ < length:
+						res[ newI ][ newJ ] += 1
 
 	return res
 
-
-if __name__ == '__main__':
-	import unittest
-
-
-	class TestSolution( unittest.TestCase ):
-
-		def test1( self ):
-			image = [ [ 1, 1, 1 ],
-			          [ 1, 7, 1 ],
-			          [ 1, 1, 1 ] ]
-
-			self.assertEquals( solution( image ), [ [ 1 ] ] )
-
-		def test2( self ):
-			image = [ [ 7, 4, 0, 1 ],
-			          [ 5, 6, 2, 2 ],
-			          [ 6, 10, 7, 8 ],
-			          [ 1, 4, 2, 0 ] ]
-
-			self.assertEquals( solution( image ), [ [ 5, 4 ],
-			                                        [ 4, 4 ] ] )
-
-
-	unittest.main()
 
 if __name__ == '__main__':
 	import unittest
