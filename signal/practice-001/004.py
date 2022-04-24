ac1433e8551c089803dc8215ca8b5a23b16eaed5
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-24 09:29:28 -0300

    auto: Tdd 004.py Green

--- signal/practice-001/004.py
+++ signal/practice-001/004.py
@@ -1,5 +1,11 @@
 def solution( numbers, k ):
-	return 0
+	count = 0
+	for i in range( len( numbers)  - 1):
+		for j in range( i + 1,  len(numbers)):
+
+			if (numbers[i] + numbers[j]) % k == 0:
+				count += 1
+	return count
 
 
 if __name__ == '__main__':
