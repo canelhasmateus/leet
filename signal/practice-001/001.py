fc0f266d576d0a1015d7050f678de8930c1e8eb8
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-24 09:15:38 -0300

    auto: Tdd 001.py Red

--- signal/practice-001/001.py
+++ signal/practice-001/001.py
@@ -1,5 +1,5 @@
-def solution( param1, param2 ):
-	...
+def solution( numbers ):
+	return []
 
 
 if __name__ == '__main__':
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 	class TestSolution( unittest.TestCase ):
 
 		def test1( self ):
-			self.assertEquals( False, True )
+			self.assertEquals( solution( [ 1, 2 , 1 , 3, 4] ), [1 , 1 ,0] )
 
 
 	unittest.main()
