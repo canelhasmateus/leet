da157d5db54f289a93aa74f5f7e50fe2b4d4a95f
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-24 15:06:23 -0300

    auto: Tdd 002.py Red

--- signal/private/002.py
+++ signal/private/002.py
@@ -1,6 +1,23 @@
 def solution( message ):
 
-	return message
+	res = []
+	lastVowel = None
+	firstVowel = 0
+	for i , c in enumerate( message ):
+
+		if c in { "a" , "e" , "i" , "o" , "u"}:
+			if lastVowel:
+				res.append( lastVowel)
+			else:
+				firstVowel = i
+			lastVowel = c
+		else:
+			res.append( c )
+
+
+
+	res.insert( firstVowel , lastVowel)
+	return "".join( res)
 
 
 if __name__ == '__main__':
@@ -11,6 +28,8 @@ if __name__ == '__main__':
 
 		def test1( self ):
 			self.assertEquals( solution( "codesignal") , "cadosegnil")
+		def test2( self ):
+			self.assertEquals( solution( "plain text") , "plean tix")
 
 
 	unittest.main()
