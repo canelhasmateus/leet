39cae5fe7e6281f67cc5b5f36f26a160f49b6bb0
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-24 09:24:41 -0300

    auto: Tdd 002.py Green

--- signal/practice-001/002.py
+++ signal/practice-001/002.py
@@ -1,5 +1,13 @@
-def solution( param1, param2 ):
-	return []
+def solution( numbers ):
+
+	counts = {}
+	for digits in map( str , numbers):
+		for character in digits:
+			counts[character] = counts.get( character , 0 ) + 1
+
+	mx = max( counts.values() )
+	res = [ int( k)  for k , v in counts.items() if v == mx ]
+	return [ i for i in sorted( res ) ]
 
 
 if __name__ == '__main__':
