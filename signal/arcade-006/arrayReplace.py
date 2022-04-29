f74477fa9fdac41f50631fe8a1c350170a077a83
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-29 08:49:41 -0300

    auto: Tdd arrayReplace.py Red

---
+++ signal/arcade-006/arrayReplace.py
@@ -0,0 +1,15 @@
+def solution( array, replaceable , replacement):
+	...
+
+
+if __name__ == '__main__':
+	import unittest
+
+
+	class TestSolution( unittest.TestCase ):
+
+		def test1( self ):
+			self.assertEquals( False, True )
+
+
+	unittest.main()
