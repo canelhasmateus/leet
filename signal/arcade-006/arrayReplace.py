caa5885bbd368a0afe04904d432f40795d73a94a
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-29 08:51:25 -0300

    auto: Tdd arrayReplace.py Green

--- signal/arcade-006/arrayReplace.py
+++ signal/arcade-006/arrayReplace.py
@@ -1,6 +1,14 @@
 def solution( array, replaceable , replacement):
-	...
 
+	res = []
+	for element in array:
+
+		if element == replaceable:
+			res.append( replacement)
+		else:
+			res.append( element)
+
+	return res
 
 if __name__ == '__main__':
 	import unittest
@@ -9,7 +17,7 @@ if __name__ == '__main__':
 	class TestSolution( unittest.TestCase ):
 
 		def test1( self ):
-			self.assertEquals( False, True )
+			self.assertEquals( solution([1,2,1] , 1 , 3), [3, 2, 3] )
 
 
 	unittest.main()
